fc4d6d8e9c57e15ddef9be2a13c9b3bfb60d81ed
waxscale <waxscale@gmail.com>
2025-08-07 02:01:06 -0700

    Replace FileBrowser with JdAreaPage

--- jdbrowser/__init__.py
+++ jdbrowser/__init__.py
@@ -1,1 +1,5 @@
 __version__ = "1.0.0"
+
+# Global pointer to the currently displayed page instance.
+# This can hold a JdAreaPage, JdIdPage or JdExtPage.
+current_page = None
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import sys
 import signal
 import re
+from typing import Union
+
 from PySide6 import QtCore
 from PySide6.QtWidgets import QApplication
-from jdbrowser.file_browser import FileBrowser
 from PySide6.QtCore import QSettings, QPoint, QSize
 from PySide6.QtGui import QFont
+
+import jdbrowser
+from jdbrowser.jd_area_page import JdAreaPage
+from jdbrowser.jd_id_page import JdIdPage
+from jdbrowser.jd_ext_page import JdExtPage
 
 # Allow Ctrl+C (SIGINT) to quit the Qt application
 signal.signal(signal.SIGINT, lambda sig, frame: QApplication.quit())
@@ -18,13 +24,16 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     app.setFont(QFont('FiraCode Nerd Font'))
-    browser = FileBrowser(start_id)
+
+    jdbrowser.current_page = JdAreaPage()
+    current_page: Union[JdAreaPage, JdIdPage, JdExtPage] = jdbrowser.current_page
+
     settings = QSettings("xAI", "jdbrowser")
     if not (settings.contains("pos") and settings.contains("size")):
-        browser.resize(1000, 600)
+        current_page.resize(1000, 600)
         screen = app.primaryScreen().geometry()
-        browser.move((screen.width() - 1000) // 2, (screen.height() - 600) // 2)
-    browser.show()
+        current_page.move((screen.width() - 1000) // 2, (screen.height() - 600) // 2)
+    current_page.show()
 
     timer = QtCore.QTimer()
     timer.timeout.connect(lambda: None)
