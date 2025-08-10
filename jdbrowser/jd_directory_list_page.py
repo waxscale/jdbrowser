cc63a14abc6749df5123c56b3fd68f8a4b6d45ea
waxscale <waxscale@gmail.com>
2025-08-10 02:00:35 -0700

    Include flow layout

--- jdbrowser/jd_directory_list_page.py
+++ jdbrowser/jd_directory_list_page.py
@@ -188,11 +188,34 @@ class JdDirectoryListPage(QtWidgets.QWidget):
         rows = cursor.fetchall()
         for idx, row in enumerate(rows):
             tag_id, label, order, icon_data = row
-            item = DirectoryItem(tag_id, label, order, icon_data, self, idx)
+            cursor.execute(
+                "SELECT tag_id, label, [order] FROM state_jd_directory_tags WHERE parent_uuid IS ? ORDER BY [order]",
+                (tag_id,),
+            )
+            child_tags = cursor.fetchall()
+            item = DirectoryItem(tag_id, label, order, icon_data, self, idx, child_tags)
             item.updateLabel(self.show_prefix)
             self.vlayout.addWidget(item)
             self.items.append(item)
         self.vlayout.addStretch(1)
+
+    def open_tag(self, tag_id, order, parent_uuid):
+        from .jd_directory_list_page import JdDirectoryListPage
+
+        s = f"{order:016d}"
+        area = int(s[0:4])
+        jd_id = int(s[4:8])
+        jd_ext = int(s[8:12])
+        new_page = JdDirectoryListPage(
+            parent_uuid=tag_id,
+            jd_area=area,
+            jd_id=jd_id,
+            jd_ext=jd_ext,
+            grandparent_uuid=parent_uuid,
+            great_grandparent_uuid=self.parent_uuid,
+        )
+        jdbrowser.current_page = new_page
+        jdbrowser.main_window.setCentralWidget(new_page)
 
     def set_selection(self, index):
         if not (0 <= index < len(self.items)):
