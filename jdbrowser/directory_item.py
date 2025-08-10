f37a4499664148f975c94772e634c9fa3a712cdd
waxscale <waxscale@gmail.com>
2025-08-09 23:05:34 -0700

    Add directory list page with database-backed creation

---
+++ jdbrowser/directory_item.py
@@ -0,0 +1,99 @@
+from PySide6 import QtWidgets, QtGui, QtCore
+from .constants import HIGHLIGHT_COLOR, HOVER_COLOR, SLATE_COLOR, TEXT_COLOR
+
+
+class DirectoryItem(QtWidgets.QWidget):
+    def __init__(self, tag_id, label, icon_data, page, index):
+        super().__init__()
+        self.tag_id = tag_id
+        self.label_text = label if label is not None else ""
+        self.page = page
+        self.index = index
+        self.isSelected = False
+        self.isHover = False
+        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_Hover)
+
+        layout = QtWidgets.QHBoxLayout(self)
+        layout.setContentsMargins(5, 5, 5, 5)
+        layout.setSpacing(10)
+
+        if icon_data:
+            pixmap = QtGui.QPixmap()
+            pixmap.loadFromData(icon_data)
+            if not pixmap.isNull():
+                self.icon = QtWidgets.QLabel()
+                rounded_pixmap = QtGui.QPixmap(240, 150)
+                rounded_pixmap.fill(QtCore.Qt.transparent)
+                painter = QtGui.QPainter(rounded_pixmap)
+                painter.setRenderHint(QtGui.QPainter.Antialiasing)
+                path = QtGui.QPainterPath()
+                path.addRoundedRect(0, 0, 240, 150, 5, 5)
+                painter.setClipPath(path)
+                scaled = pixmap.scaled(
+                    240,
+                    150,
+                    QtCore.Qt.AspectRatioMode.KeepAspectRatio,
+                    QtCore.Qt.TransformationMode.SmoothTransformation,
+                )
+                painter.drawPixmap(0, 0, scaled)
+                painter.end()
+                self.icon.setPixmap(rounded_pixmap)
+                self.icon.setFixedSize(240, 150)
+                self.icon.setStyleSheet("background-color: transparent;")
+            else:
+                self.icon = QtWidgets.QFrame()
+                self.icon.setFixedSize(240, 150)
+                self.icon.setStyleSheet(
+                    f"background-color: {SLATE_COLOR}; border-radius: 5px;"
+                )
+        else:
+            self.icon = QtWidgets.QFrame()
+            self.icon.setFixedSize(240, 150)
+            self.icon.setStyleSheet(
+                f"background-color: {SLATE_COLOR}; border-radius: 5px;"
+            )
+        layout.addWidget(self.icon)
+
+        self.right = QtWidgets.QWidget()
+        right_layout = QtWidgets.QVBoxLayout(self.right)
+        right_layout.setContentsMargins(0, 0, 0, 0)
+        right_layout.setSpacing(2)
+
+        self.label = QtWidgets.QLabel(self.label_text)
+        self.label.setAlignment(
+            QtCore.Qt.AlignmentFlag.AlignLeft | QtCore.Qt.AlignmentFlag.AlignTop
+        )
+        font = self.label.font()
+        font.setPointSize(int(font.pointSize() * 1.2))
+        font.setBold(True)
+        self.label.setFont(font)
+        self.label.setStyleSheet(f"color: {TEXT_COLOR};")
+        right_layout.addWidget(self.label)
+        right_layout.addStretch(1)
+        layout.addWidget(self.right)
+
+        self.updateStyle()
+
+    def updateStyle(self):
+        bg = HIGHLIGHT_COLOR if self.isSelected else (
+            HOVER_COLOR if self.isHover else "transparent"
+        )
+        self.setStyleSheet(f"background-color: {bg}; border-radius: 5px;")
+
+    def enterEvent(self, event):
+        self.isHover = True
+        self.updateStyle()
+        self.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
+        super().enterEvent(event)
+
+    def leaveEvent(self, event):
+        self.isHover = False
+        self.updateStyle()
+        self.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.ArrowCursor))
+        super().leaveEvent(event)
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton and self.page:
+            self.page.set_selection(self.index)
+        super().mousePressEvent(event)
+
