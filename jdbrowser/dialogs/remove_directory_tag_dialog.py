b795d8713f00f991815fbe0c53641a56f7bcf8dd
waxscale <waxscale@gmail.com>
2025-08-10 17:37:33 -0700

    Wrap remove directory tag dialog message

--- jdbrowser/dialogs/remove_directory_tag_dialog.py
+++ jdbrowser/dialogs/remove_directory_tag_dialog.py
@@ -9,12 +9,17 @@ class RemoveDirectoryTagDialog(QtWidgets.QDialog):
         layout = QtWidgets.QVBoxLayout(self)
         layout.setSpacing(10)
         layout.setContentsMargins(10, 10, 10, 10)
+        layout.setSizeConstraint(QtWidgets.QLayout.SetFixedSize)
 
         message = (
             f"Are you sure you want to remove the tag '{tag_name}' from the directory '{directory_name}'?"
         )
         self.message = QtWidgets.QLabel(message)
+        self.message.setWordWrap(True)
         self.message.setStyleSheet(f"color: {TEXT_COLOR};")
+        self.message.setSizePolicy(
+            QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred
+        )
         layout.addWidget(self.message)
 
         buttons = QtWidgets.QDialogButtonBox(
@@ -39,4 +44,5 @@ class RemoveDirectoryTagDialog(QtWidgets.QDialog):
         layout.addWidget(buttons)
 
         self.setStyleSheet(f"background-color: {BACKGROUND_COLOR};")
+        self.adjustSize()
 
